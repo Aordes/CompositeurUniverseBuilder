ca2639bbb26062bbfa8b03e2e9f44c7edc723751
Soren SZABO <s.szabo@student.isartdigital.com>
2019-10-03 12:07:38 +0200

    [Add] ScriptTemplateSettings + ToolBar script

---
+++ Assets/_CompositeurUniverseBuilder/Scripts/ToolBar.cs
@@ -0,0 +1,28 @@
+///-----------------------------------------------------------------
+///   Author : Soren SZABO                    
+///   Date   : 03/10/2019 11:36
+///-----------------------------------------------------------------
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Com.Docaret.CompositeurUniverseBuilder {
+
+    public class ToolBar : MonoBehaviour {
+
+        [SerializeField] private Button btnAddMetaToMainFolder;
+        [SerializeField] private Button btnAddMetaToSelection;
+        [SerializeField] private Button btnRenameMainFolder;
+        [SerializeField] private Button btnPreviewToMainfolder;
+        [SerializeField] private Button btnDeleteMainfolder;
+        [SerializeField] private Button btnDeleteSelection;
+
+        private void Start () {
+            
+        }
+
+        private void Update () {
+            
+        }
+    }
+}
