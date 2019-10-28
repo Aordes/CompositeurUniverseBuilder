35118e32363f51408819ed34b3c79308c107dd2a
Soren SZABO <s.szabo@student.isartdigital.com>
2019-10-28 17:53:26 +0100

    [Add] Complete ImportUniverse method

--- Assets/_CompositeurUniverseBuilder/Scripts/FileImporter.cs
+++ Assets/_CompositeurUniverseBuilder/Scripts/FileImporter.cs
@@ -3,8 +3,10 @@
 ///   Date   : 18/10/2019 14:52
 ///-----------------------------------------------------------------
 
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using UnityEngine;
 
 namespace Com.Docaret.CompositeurUniverseBuilder {
@@ -12,21 +14,21 @@ namespace Com.Docaret.CompositeurUniverseBuilder {
     public struct UniverseStruct
     {
         public Sprite background;
-        public List<FolderStruct> folders;
-        public List<FileStruct> files;
+        public List<UniverseFolderStruct> folders;
+        public List<UniverseFileStruct> files;
     }
 
-    public struct FolderStruct
+    public struct UniverseFolderStruct
     {
         public Sprite icon;
-        public List<FolderStruct> subFolders;
-        public List<FileStruct> files;
+        public List<UniverseFolderStruct> subFolders;
+        public List<UniverseFileStruct> files;
     }
 
-    public struct FileStruct
+    public struct UniverseFileStruct
     {
         public Sprite preview;
-        public FileInfo meta;
+        public string meta;
         public FileInfo file;
     }
 
@@ -73,40 +75,30 @@ namespace Com.Docaret.CompositeurUniverseBuilder {
                 }
             }
 
-            universe.folders = new List<FolderStruct>();
+            universe.folders = new List<UniverseFolderStruct>();
             for (int i = 0; i < directories.Length; i++)
             {
                 GetFolderStruct(directories[i], universe.folders);
             }
 
+            Debug.Log(universe);
         }
 
-        public static void GetFolderStruct(DirectoryInfo folder, List<FolderStruct> list)
+        public static void GetFolderStruct(DirectoryInfo folder, List<UniverseFolderStruct> list)
         {
             if (folder.Name.StartsWith(FileTypes.UNDERSCORE) || folder.Name.EndsWith(FileTypes.CONTENT_FOLDER))
                 return;
 
-            FolderStruct currentFolder;
+            UniverseFolderStruct currentFolder;
             Debug.Log(folder.Name + " has " + folder.GetFiles().Length + " files \n" + folder.GetDirectories().Length);
 
-            currentFolder = new FolderStruct();
-            currentFolder.icon = GetItemPreview(folder, FileTypes.FOLDER_ICON + FileTypes.ALL_EXTENSION);
-
-            currentFolder.files = new List<FileStruct>();
-            FileInfo[] files = folder.GetFiles();
-            for (int i = 0; i < files.Length; i++)
+            currentFolder = new UniverseFolderStruct
             {
-                if (!(files[i].Name.Contains(FileTypes.FOLDER_ICON) || files[i].Name.Contains(FileTypes.META)))
-                    currentFolder.files.Add(
-                        new FileStruct()
-                        {
-                            file = files[i],
-                            preview = GetItemPreview(files[i].Directory, files[i].Name)
-                        }
-                    );
-            }
+                icon = GetItemPreview(folder, FileTypes.FOLDER_ICON + FileTypes.ALL_EXTENSION),
+                files = GetFileStruct(folder)
+            };
 
-            currentFolder.subFolders = new List<FolderStruct>();
+            currentFolder.subFolders = new List<UniverseFolderStruct>();
             DirectoryInfo[] directories = folder.GetDirectories();
             for (int i = 0; i < directories.Length; i++)
             {
@@ -116,10 +108,49 @@ namespace Com.Docaret.CompositeurUniverseBuilder {
             list.Add(currentFolder);
         }
 
+        private static List<UniverseFileStruct> GetFileStruct(DirectoryInfo folder)
+        {
+            List<UniverseFileStruct> universeFiles = new List<UniverseFileStruct>();
+
+            FileInfo[] files = folder.GetFiles();
+            DirectoryInfo fileDirectory;
+
+            string fileName;
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                fileName = files[i].Name;
+                fileDirectory = files[i].Directory;
+
+                if (!(fileName.Contains(FileTypes.FOLDER_ICON) || fileName.Contains(FileTypes.META)))
+                    universeFiles.Add(
+                        new UniverseFileStruct()
+                        {
+                            file = files[i],
+                            preview = GetItemPreview(fileDirectory, fileName),
+                            meta = GetItemMeta(fileDirectory, fileName)
+                        }
+                    );
+            }
+
+            return universeFiles;
+        }
+
+        private static string GetItemMeta(DirectoryInfo directoryInfo, string fileName)
+        {
+            FileInfo[] files = directoryInfo.GetFiles(fileName);
+            
+            if (files.Length != 0)
+            {
+                return File.ReadAllText(files[0].FullName, Encoding.UTF8);
+            }
+
+            return null;
+        }
+
         public static Sprite GetItemPreview(DirectoryInfo directoryInfo, string fileName)
         {
             FileInfo[] files = directoryInfo.GetFiles(fileName);
-
             Sprite image;
 
             if (files.Length != 0)
