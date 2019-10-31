a2157f86cc2f1af289275c036b46f7c00265f87d
Adrien Bordes <adrien-bordes@hotmail.fr>
2019-10-31 14:57:35 +0100

    [Add] Extentions Icon

---
+++ Assets/_CompositeurUniverseBuilder/Scripts/ExtentionIcons.cs
@@ -0,0 +1,43 @@
+///-----------------------------------------------------------------
+/// Author : 
+/// Date : 31/10/2019 14:19
+///-----------------------------------------------------------------
+
+using UnityEngine;
+
+namespace Com.Docaret.CompositeurUniverseBuilder
+{
+	public class ExtentionIcons : MonoBehaviour {
+
+        public Sprite avi;
+        public Sprite gif;
+        public Sprite jpg;
+        public Sprite mp3;
+        public Sprite mp4;
+        public Sprite pdf;
+        public Sprite png;
+        public Sprite powerpoint;
+        public Sprite txt;
+        public Sprite zip;
+
+        //public static Sprite GetSpriteFromExtention(string extention)
+        //{
+        //    if (extention == ".avi")
+        //    {
+        //        return avi;
+        //    }
+        //    else if (extention == ".gif") return gif;
+        //    else if (extention == ".jpg") return jpg;
+        //    else if (extention == ".mp3") return mp3;
+        //    else if (extention == ".mp4") return mp4;
+        //    else if (extention == ".pdf") return pdf;
+        //    else if (extention == ".png") return png;
+        //    else if (extention == ".pptx") return powerpoint;
+        //    else if (extention == ".ppt") return powerpoint;
+        //    else if (extention == ".txt") return txt;
+        //    else if (extention == ".zip") return zip;
+        //    else return null;
+        //}
+	
+	}
+}
